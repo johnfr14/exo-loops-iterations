49e4880c083eb4f02f76d88b017a4432ea9cb274
johnfr14 <jonathan.alyra@gmail.com>
2021-02-26 17:36:02 +0100

    lolololol

--- onlyMultipleOf3And7.js
+++ onlyMultipleOf3And7.js
@@ -1,6 +1,3 @@
-let three = 0
-let seven = 0
-
 for(let i = 1; i <= 1000; i++){
   i % 3 === 0 ? console.log(`${i} est bien un multiple de 3\n3 x ${i / 3}\n`) : ''
   i % 7 === 0 ? console.log(`${i} est bien un multiple de 7\n7 x ${i / 7}\n`) : ''
